5d33bf1c6c386ad521a6e752bb9ec3c1dbc357af
Tuyishime Kyrie <tuyishimekyrie@gmail.com>
2024-07-13 12:36:21 +0200

    add javascript logic in the app

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
+// Javascript
 const baseInput = document.querySelector(".base__input");
 const input = document.querySelector(".base__input input");
 const baseBtn = document.querySelector(".base__input .bt");
 const result = document.querySelector(".result");
 const error = document.querySelector(".base__input .error");
-console.log(input);
 baseBtn.addEventListener("click", (e) => {
   e.preventDefault();
   let output = input.value;
   console.log(input.value);
   result.innerHTML = output;
-  // output = ''
   if (output) {
     result.innerHTML = output;
   } else {
